91e9127a249d47ef681b6ec49db54b69bf7b6c6a
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2022-07-27 18:47:27 +0200

    add easycredit logo component

---
+++ src/components/easycredit-logo/easycredit-logo.stories.tsx
@@ -0,0 +1,24 @@
+import { buildAttributes } from '../../../.storybook/helpers'
+
+export default {
+  title: "Marketing/Logo",
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "Das Logo vereinfacht die Einbindung des ratenkauf by easyCredit Logos und kann statt eines img-Tags eingesetzt werden",
+      }
+    }    
+  },
+  argTypes: {},
+};
+
+
+let args = {
+  alt: ''
+}
+
+const Template = (args) => `<easycredit-logo ${buildAttributes(args).join(' ')} />`
+export const Logo = Template.bind({});
+Logo.storyName = 'Logo'
+Logo.args = args
