a43b75eb565dd8dd6b2a7dcc7e22a4f666c7194e
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2023-09-12 16:56:56 +0200

    update docs & version bump

--- src/components/easycredit-merchant-manager/easycredit-merchant-manager.stories.tsx
+++ src/components/easycredit-merchant-manager/easycredit-merchant-manager.stories.tsx
@@ -7,26 +7,7 @@ export default {
       description: {
         component: `Der Merchant Manager ermöglicht dem Händler eine einfache Transaktionsverarbeitung über sein Shop-Backend. Typischerweise wird er im Shop-System in der Bestelldetailansicht angezeigt oder im Bereich der Transaktionen einer Bestellung.
         
-Um die Transaktionen abrufen zu können, müssen für den Merchant Manager die Endpunkte und die Authentifizerung angegeben werden. Dies kann z.B. global in der Seite über ein Skript-Tag passieren:
-
-        <script>
-    window.ratenkaufbyeasycreditOrderManagementConfig = {
-        endpoints: {  // {transactionId} is automatically replaced with the tx-id or tx-id's (list)
-            list: 'api/transactions/{transactionId}',
-            get: 'api/transaction/{transactionId}', 
-            capture: 'api/transaction/{transactionId}/capture',
-            refund: 'api/transaction/{transactionId}/refund'
-        },
-        request_config: {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + Context.api.authToken.access
-            }
-        }
-    }
-    </script>
-
-Das Objekt request_config entspricht dabei der init-Option von fetch(resource [, init]).
+Um die Transaktionen abrufen zu können, müssen für den Merchant Manager die Endpunkte und die Authentifizerung angegeben werden. Dies kann z.B. global in der Seite über ein Skript-Tag passieren, siehe [Konfiguration über globale Variable](?path=/docs/getting-started-configuration--docs).
         `,
       }
     }    
